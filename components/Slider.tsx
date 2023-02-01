f523ba0f43219a3412f66d4c7824856b2671db47
FlorencioQuelca <florens.lonwe@gmail.com>
2023-02-01 11:37:24 -0400

    fix: arrowflex

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -9,40 +9,46 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import NextArrow from './NextArrow';
 type Props = {};
 const Slider = () => {
   return (
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={10}
-      slidesPerView={3}
-      navigation
-      onSwiper={(swiper) => console.log(swiper)}
-      className="min-h-full"
-    >
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <EventCard />
-      </SwiperSlide>
-      .
-    </Swiper>
+    <div className="flex flex-row items-center">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        slidesPerView={3}
+        // navigation
+        onSwiper={(swiper) => console.log(swiper)}
+        className="min-h-full"
+      >
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCard />
+        </SwiperSlide>
+        .
+      </Swiper>
+      <div className="">
+        <NextArrow />
+      </div>
+    </div>
   );
 };
 
